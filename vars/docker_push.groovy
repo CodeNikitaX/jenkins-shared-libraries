11afe05e60f6b521758df8d0963d29bf094adad2
NL <leuvanikita@gmail.com>
2025-04-20 19:44:16 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -5,7 +5,7 @@ def call(String project, String image){
        usernameVariable: "dockerHubUser"
        )]){
            sh "docker login -u  ${env.dockerHubUser} -p ${env.dockerHubPass}"
-           sh "docker image tag ${image} ${env.dockerHubUser}/${project}"
-           sh "docker push ${env.dockerHubUser}/${project}"
+           sh "docker image tag ${image} ${env.dockerHubUser}/${project}:latest"
+           sh "docker push ${env.dockerHubUser}/${project}:latest"
      }
 }
